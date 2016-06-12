bf3c1f3fa636cee7ef467e4ba41af4ffce43625e
Marcus Bernales <mboperator@gmail.com>
2016-06-11 19:36:17 -0700

    removed other examples

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import SingleModuleExample from './single-module/app';
 import MultiModuleExample from './multi-module/app';
-import MiddlewareExample from './module-with-middleware/app';
 
 const Examples = () => (
   <div>
-    <SingleModuleExample />
     <MultiModuleExample />
-    <MiddlewareExample />
   </div>
 );
 
