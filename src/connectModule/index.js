9f5487c50439db2a382790357a0b2db218265c21
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2016-06-19 20:11:14 -0700

    Enable default selector.

--- src/connectModule/index.js
+++ src/connectModule/index.js
@@ -1,8 +1,25 @@
 import connectModules from './connectModules';
 
-export const connectModule = (selector, modules) => Component => {
-  const formatted = Array.isArray(modules) ? modules : [modules];
-  return connectModules(selector, formatted, Component);
+const createModuleSelector = modules => {
+  const moduleNames = modules.map(module => module.name);
+  // If there's only one module (as in most cases), use a simple accessor
+  if (moduleNames.length === 1) {
+    const name = moduleNames[0];
+    return state => state[name];
+  }
+  // If https://github.com/reactjs/react-redux/issues/407 makes it, reselect can be used here
+  return state => moduleNames.reduce(
+    (props, name) => Object.assign(props, state[name]), {});
+};
+
+export const connectModule = (selector, modules) => {
+  let formattedSelector = selector;
+  let formattedModules = Array.isArray(modules) ? modules : [modules];
+  if (typeof selector === 'object' && typeof modules === 'undefined') {
+    formattedModules = Array.isArray(selector) ? selector : [selector];
+    formattedSelector = createModuleSelector(formattedModules);
+  }
+  return Component => connectModules(formattedSelector, formattedModules, Component);
 };
 
 export default connectModule;
