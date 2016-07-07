595d52ae95d73d26b4b5f2111209ea281ac87558
Marcus Bernales <mboperator@gmail.com>
2016-07-06 22:52:52 -0700

    Modified examples to use type instead of action

--- examples/dynamic-reducer/modules/todo.js
+++ examples/dynamic-reducer/modules/todo.js
@@ -10,7 +10,7 @@ export default createModule({
   initialState: List(),
   transformations: [
     {
-      action: 'CREATE',
+      type: 'CREATE',
       payloadTypes: {
         todo: shape({
           description: string.isRequired,
@@ -19,14 +19,14 @@ export default createModule({
       reducer: (state, { payload: { todo } }) => state.push(fromJS(todo)),
     },
     {
-      action: 'DESTROY',
+      type: 'DESTROY',
       payloadTypes: {
         index: number.isRequired,
       },
       reducer: (state, { payload: { index } }) => state.delete(index),
     },
     {
-      action: 'UPDATE',
+      type: 'UPDATE',
       payloadTypes: {
         index: number.isRequired,
         todo: shape({
