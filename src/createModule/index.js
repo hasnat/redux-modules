3a0e742fca5fa09d7c8811ac17b080a9ea69fff8
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2016-09-27 10:05:03 -0700

    Use lodash.

--- src/createModule/index.js
+++ src/createModule/index.js
@@ -1,9 +1,8 @@
-import camelize from 'camel-case';
 import createAction from './createAction';
+import { camelCase, map } from 'lodash';
 import parsePayloadErrors from '../middleware/parsePayloadErrors';
 import propCheck from '../middleware/propCheck';
 
-const defaultMiddleware = [parsePayloadErrors];
 const defaultReducer = state => state;
 
 const applyReducerEnhancer = (reducer, enhancer) => {
@@ -20,101 +19,76 @@ const formatTransformation = (name, { action, type, ...transformation }) => ({
   ...transformation,
 });
 
-const parseTransformation = (type, transformation) => {
-  if (typeof transformation === 'function') {
-    return {
-      type,
-      reducer: transformation,
-    };
+function parseTransformations(transformations) {
+  function parseTransformation(type, transformation) {
+    return (typeof transformation === 'function') ?
+      { type, reducer: transformation } :
+      { type, ...transformation };
   }
-  return {
-    type,
-    ...transformation,
-  };
-};
+  return Array.isArray(transformations) ?
+    transformations :
+    map(transformations, parseTransformation);
+}
 
-const parseTransformations = transformations => {
-  if (Array.isArray(transformations)) {
-    return transformations;
-  }
-  const finalTransformations = [];
-  const keys = Object.keys(transformations);
-  for (let i = 0; i < keys.length; ++i) {
-    const key = keys[i];
-    const transformation = transformations[key];
-    const finalTransformation = parseTransformation(key, transformation);
-    finalTransformations.push(finalTransformation);
-  }
-  return finalTransformations;
-};
-
-export const createModule = ({
-  initialState,
-  reducerEnhancer,
+export default function createModule({
   composes = [],
+  initialState,
   name,
+  reducerEnhancer,
   selector,
-  transformations,
-}) => {
+  transformations }) {
+  const finalTransformations = parseTransformations(transformations);
   const actions = {};
   const constants = {};
   const reducerMap = {};
-
-  const finalTransformations = parseTransformations(transformations);
-
   for (let i = 0; i < finalTransformations.length; ++i) {
-    const transformation = formatTransformation(name, finalTransformations[i]);
     const {
-        action,
-        type,
-        namespaced = true,
-        formattedConstant,
-        reducer,
-        payloadTypes,
-        middleware = [],
-      } = transformation;
+      action,
+      formattedConstant,
+      middleware = [],
+      namespaced = true,
+      payloadTypes,
+      reducer,
+      type,
+    } = formatTransformation(name, finalTransformations[i]);
 
-    const finalMiddleware = [... defaultMiddleware, ... middleware];
+    const finalMiddleware = [parsePayloadErrors, ...middleware];
     const constant = namespaced ? formattedConstant : type;
 
     // DEPRECATION WARNINGS
     if (process.env.NODE_ENV !== 'production') {
       action && console.warn(
-      `${constant}::`,
-      'The `action` key is deprecated. Use `type` instead.'
-    );
+        `${constant}::`,
+        'The `action` key is deprecated. Use `type` instead.'
+      );
 
       if (payloadTypes) {
         console.warn(
           `${constant}::`,
           'The `payloadTypes` key is deprecated. Use middleware.propCheck instead'
         );
-        const propChecker = propCheck(transformation.payloadTypes);
+        const propChecker = propCheck(payloadTypes);
         finalMiddleware.push(propChecker);
       }
     }
 
-    const camelizedActionName = camelize(type);
+    const camelizedActionName = camelCase(type);
     actions[camelizedActionName] = createAction(constant, finalMiddleware);
     constants[camelizedActionName] = constant;
-    reducerMap[constant] = applyReducerEnhancer(reducer, reducerEnhancer);
+    reducerMap[constant] = reducer;
   }
-
   const reducer = (state = initialState, action) => {
     const localReducer = reducerMap[action.type] || defaultReducer;
     return [
       localReducer,
-      ... composes,
+      ...composes,
     ].reduce((newState, currentReducer) => currentReducer(newState, action), state);
   };
-
   return {
     actions,
     constants,
     name,
-    reducer,
+    reducer: applyReducerEnhancer(reducer, reducerEnhancer),
     selector,
   };
-};
-
-export default createModule;
+}
