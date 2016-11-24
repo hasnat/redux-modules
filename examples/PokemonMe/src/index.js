1894aa095c026cbf1723fa8845aa44d52eba2f5e
Marcus Bernales <mboperator@gmail.com>
2016-11-23 18:01:52 -0800

    Updated pokemon example

--- examples/PokemonMe/src/index.js
+++ examples/PokemonMe/src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ModuleProvider } from '../../../lib';
 import { createStore } from 'redux';
 import { install, combineReducers } from 'redux-loop';
+import { ModuleProvider } from '../../../lib';
 import App from './App';
+import exportModule from './module';
 import './index.css';
 
 const store = createStore(s => s, {}, install());
 
-ReactDOM.render(
+export const module = exportModule;
+export const Component = App;
+export const Bootstrapped = () => (
   <ModuleProvider store={store} combineReducers={combineReducers}>
-    <div style={{display: 'flex', width: '100%', height: '100%'}}>
+    <div style={{ display: 'flex', width: '100%', height: '100%' }}>
       <App />
     </div>
-  </ModuleProvider>,
-  document.getElementById('root')
+  </ModuleProvider>
 );
+
+const mountPoint = document.getElementById('pokemon-example');
+if (mountPoint) {
+  ReactDOM.render(
+    <Bootstrapped />,
+    mountPoint,
+  );
+}
