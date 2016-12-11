8765e0e692447d39e186d9271ffc7547e3219ebb
Marcus Bernales <mboperator@gmail.com>
2016-12-11 15:24:27 +0800

    Removed propCheck logic, added invariant to peer dependencies, added deprecation warning

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,7 @@
-import exportPropCheck from './propCheck';
+import invariant from 'invariant';
 
 // eslint-disable-next-line import/prefer-default-export
-export const propCheck = exportPropCheck;
+export const propCheck = () => (action) => {
+  invariant(true, 'redux-modules | middleware.propCheck is deprecated! Please use github.com/mboperator/redux-modules-middleware instead.');
+  return action;
+};
