4a479c61aa81605bb183ecebfbe0deaa47a53f50
Marcus Bernales <mboperator@gmail.com>
2017-01-16 14:29:13 -0800

    Removed middleware altogether, major bump

--- src/middleware/index.js
+++
@@ -1,7 +0,0 @@
-import invariant from 'invariant';
-
-// eslint-disable-next-line import/prefer-default-export
-export const propCheck = () => (action) => {
-  invariant(true, 'redux-modules | middleware.propCheck is deprecated! Please use github.com/mboperator/redux-modules-middleware instead.');
-  return action;
-};
